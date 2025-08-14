738dc7552b0404d8695d9e11c7e2a2020782a3f9
V0giK <119131833+V0giK@users.noreply.github.com>
2025-08-14 23:16:09 +0200

    LICENCE

--- software/sTankTeil_Remote/src/snakeGame.cpp
+++ software/sTankTeil_Remote/src/snakeGame.cpp
@@ -1,3 +1,20 @@
+/*
+ * Hauptprogramm zur Steuerung einer Arduino-basierten Tanklösung
+ *
+ * Copyright (C) 2025 V0giK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "snakeGame.h"
 
 SnakeGame::SnakeGame() : snake_length(3), direction(0), game_running(false), timer(nullptr), prev_screen(nullptr), game_over_timer(nullptr), speed(500), score(0) {
